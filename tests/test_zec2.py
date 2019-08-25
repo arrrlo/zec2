546e83ff53bd57f6b8472f107e4f8d3a4dea7ba8
Ivan Arar <ivan.arar@gmail.com>
2019-08-25 12:55:42 +0200

    tests

---
+++ tests/test_zec2.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestGoogleApi(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
